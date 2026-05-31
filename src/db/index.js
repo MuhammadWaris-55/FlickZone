79eeab86dd1c09d1cb52c062133c029da25e781b
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-31 11:19:10 +0500

    Connect DataBase

--- src/db/index.js
+++ src/db/index.js
@@ -6,9 +6,13 @@ const connectDB = async () => {
       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}
         /${DB_NAME}`)
         console.log(`\n MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`);
+        //.connection.host just tells you which server/machine your app connected to.
     } catch (error) {
         console.log("MongoDB Connection Error :" + error)
         process.exit(1)
+        //process is Node's way of referring to your currently running app.
+        //process.exit() means → "stop the app right now"
+        //The number 1 is the exit code, which signals that the app stopped due to an error
     }
 }
 

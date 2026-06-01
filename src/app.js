303d1d0d6720055bbb5467cfc9fc19970c4ebffa
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-01 15:18:24 +0500

    Config App

--- src/app.js
+++ src/app.js
@@ -11,4 +11,23 @@ app.use(cors({
     credentials: true
 }))
 
+
+//Data will come from many places like from URl , JSON , or from req.body means form so these are settings to handle that data or configurations to handle the data  .
+
+app.use(express.json({ //This means accepting json data
+    limit: "20kb" //this shows that how much data it can accept it usually depends on power of server or etc
+}))
+
+app.use(express.urlencoded({ //This is for to encode the url that user enters because url not comes same everytime , it includes special characters so this will encode it
+    extended: true,
+    limit: "20kb"
+}))
+
+app.use(express.static("public")) //this will accept file and folders or any asset like img or vid etc and store it on server in public folder 
+
+app.use(cookieParser()) //the work of cookie parser is that we can access cookies of server (user's browser) and sets it and operates CRUD operation on thier cookies
+
+//A cookie is a small piece of data the server sends to the client, which the browser stores and automatically sends back with every subsequent request to that server.
+// A cookie is just a small text file the server tells your browser to save.
+
 export { app }

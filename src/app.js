3a351fe6992a0932f63c4cfa7703fab5b769bb13
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-01 14:49:54 +0500

    CORS config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
-import express from "express"
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 const app = express();
+
+//Configuring CORS
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
 export { app }

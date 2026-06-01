0867955f858f5260e3ac1a3dd2a0b599c7c57733
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-01 11:13:36 +0500

    Make App

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`App is Running on port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection Failed !!" + err);
+    
+})
 
 
 

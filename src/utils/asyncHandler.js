419033dbe9bc6c0eaa5542a21f6b706b5c6c42cf
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-03 12:03:53 +0500

    Wrapper Function

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -9,18 +9,3 @@ const asyncHandler = (requestHandler) => {
 
 
 export { asyncHandler }
-
-
-
-//Another way of making it
-//This is a wrapper function made with async await which we gonna use everywhere in code
-// const asyncHandler = (fn) => async (req, res, next) => { //Higer Order Function
-//     try {
-//         await fn(req, res, next)
-//     } catch (err) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// }

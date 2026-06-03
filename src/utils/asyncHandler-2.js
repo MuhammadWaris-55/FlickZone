bcfd79900da5ffbc4687781c25ef5a763918bcd7
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-03 12:04:19 +0500

    Wrapper Function

--- src/utils/asyncHandler-2.js
+++ src/utils/asyncHandler-2.js
@@ -1,6 +1,6 @@
 //Another way of making wrapper Function
 //This is a wrapper function made with async await which we gonna use everywhere in code
-// const asyncHandler = (fn) => async (req, res, next) => { //Higer Order Function
+// const asyncHandler2 = (fn) => async (req, res, next) => { //Higer Order Function
 //     try {
 //         await fn(req, res, next)
 //     } catch (err) {
@@ -10,3 +10,5 @@
 //         })
 //     }
 // }
+
+// export { asyncHandler2 }

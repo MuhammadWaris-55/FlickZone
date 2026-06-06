2f110184af02060e570dca016164eba09a2fbf86
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-06 15:06:36 +0500

    Encrypt Password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,11 +52,18 @@ const userSchema = new Schema(
 //We cannot give the callback with arrow function because in arrow function we cannot use "this" keyword for referance but here giving referance is important
 //"save" means the logic we r writing in the function will execute just right before saving the data
 //using "next" because we r using middleware
+
 userSchema.pre("save", async function (next) {
     //using if condtion to avoid to run this hook everytime on every save , now this will only run if password is modified 
     if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 10);
+    this.password = bcrypt.hash(this.password, 10);
   }
   next();
 });
+
+//Making a custom method to ask from user that password is correct?
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password , this.password)
+}
+
 export const User = mongoose.model("User", userSchema);

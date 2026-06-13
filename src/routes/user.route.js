ddced9f77bee45d60b7040fe17b89304531fec6e
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-13 12:28:02 +0500

    add route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,5 +4,7 @@ import { registerUser } from "../controllers/user.controller.js";
 const router = Router()
 
 router.route("/register").post(registerUser)
+//Now the url will look like 
+//http:localhost:8000/users/register
 
 export default router

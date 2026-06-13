242fa5ea3348f10c751f61175b6ffc83730dd050
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-13 12:31:01 +0500

    add route

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(cookieParser()) //the work of cookie parser is that we can access cookie
 import userRouter from "./routes/user.route.js";
 
 //routes declaration
-app.use("/users" , userRouter) //using app.use because router is in another file
+app.use("/api/v1/users" , userRouter) //using app.use because router is in another file
 //if we go to /users it will redirect us to userRouter
 
 export { app }

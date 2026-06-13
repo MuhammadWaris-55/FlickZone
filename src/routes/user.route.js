4e4fe02ec08923d3a41866d6caacac0ebb4b7fbe
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-13 15:34:21 +0500

    fix bugs

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,6 +5,6 @@ const router = Router()
 
 router.route("/register").post(registerUser)
 //Now the url will look like 
-//http://localhost:5000/api/v1/users/register
+//http://localhost:8000/api/v1/users/register
 
 export default router

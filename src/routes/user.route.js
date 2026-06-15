72f642df65b88e78b75f9631ea3034e1c0e71c8b
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-15 12:43:46 +0500

    user registration controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,24 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+
+//way of injecting middleware
+router.route("/register").post(
+    upload.fields([  //making this to handle multiple files 
+        {
+            name: "avatar", //this name should be same when make this field in frontend
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 //Now the url will look like 
 //http://localhost:8000/api/v1/users/register
 

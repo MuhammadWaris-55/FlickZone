daec6ab2e0e64bbb86c89f667526635b5fc8a8da
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-17 23:27:30 +0500

    testing & fix bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,7 +44,12 @@ const registerUser = asyncHandler(async (req, res) => {
     // check for images, avatar , we got req.files from multer(middleware)
     //this means If files were uploaded, and if there's an avatar, give me the local disk path of the first one
    const avatarLocalPath = req.files?.avatar?.[0]?.path
-    const coverImageLocalPath = req.files?.coverImage?.[0]?.path  
+    // const coverImageLocalPath = req.files?.coverImage?.[0]?.path 
+    
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+      coverImageLocalPath = req.files.coverImage[0].path  
+    }
 
     //check for avatar
     if (!avatarLocalPath) {

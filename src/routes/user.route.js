33aad05c726ca8850a8e1da35a7498fe693d03db
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-21 00:38:59 +0500

    add login & logout routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
@@ -21,5 +21,11 @@ router.route("/register").post(
 )
 //Now the url will look like 
 //http://localhost:8000/api/v1/users/register
+
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(logoutUser)
 
 export default router

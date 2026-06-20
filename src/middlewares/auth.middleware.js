08eaf75260ea01a50a512491686db0e950a6a03b
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-21 00:15:29 +0500

    add auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
 
 
 export const verifyJWT = asyncHandler(async(req, res, next) => {
@@ -8,4 +9,6 @@ export const verifyJWT = asyncHandler(async(req, res, next) => {
     if (!token) {
         throw new ApiError(401, "Unauthorized request")
     }
+
+    const decodedTokenInfo = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 })

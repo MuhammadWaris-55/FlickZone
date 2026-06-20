b08eded8bcb1f5c94635cc8f25233e526ad6c39c
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-20 23:51:56 +0500

    add logoutUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -176,10 +176,11 @@ const loginUser = asyncHandler( async (req, res) => {
 })
 
 const logoutUser = asyncHandler( async (req, res) => {
-    
+
 })
 
 export { 
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }

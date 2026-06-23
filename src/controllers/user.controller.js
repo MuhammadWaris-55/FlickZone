37cee3c9a41db40bd8f9d093287dd5da8587815a
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-23 14:29:29 +0500

    add changeCurrentPassword function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -258,6 +258,11 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    
+})
+
+
 export {
     registerUser,
     loginUser,

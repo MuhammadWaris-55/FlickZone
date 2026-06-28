d416f18f4d69f6779b496bc7ce8b3842d3f1c839
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-28 12:11:23 +0500

    feat: enhance getUserChannelProfile with additional user fields in projection

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -424,6 +424,19 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
                     }   
                 }
             }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+
+            }
         }
     ])
 })

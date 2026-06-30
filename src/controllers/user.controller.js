1656a5b665a5a26e97de2390744801c84dd712e1
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-06-30 15:57:15 +0500

    feat: enhance getWatchHistory to return a structured response with success message

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -513,6 +513,16 @@ const getWatchHistory = asyncHandler(async (req, res) => {
             }
         }
     ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch History fetched successfully"
+        )
+    )
 })
 
 

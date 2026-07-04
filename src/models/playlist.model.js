6610f387a362cfc7978c13b481fdb2562713059f
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-04 12:04:28 +0500

    feat: create initial playlist model schema

---
+++ src/models/playlist.model.js
@@ -0,0 +1,11 @@
+import mongoose, {Schema} from "mongoose";
+
+const playlistSchema = new Schema(
+    {
+
+    },
+    {timestamps: true}
+)
+
+
+export const Playlist = mongoose.model("Playlist", playlistSchema)

3a32ab19d0cc2b269d656a5844f30f7faa08ad6e
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-04 12:17:40 +0500

    feat: define tweet model schema with owner and content fields

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -2,7 +2,14 @@ import mongoose, {Schema} from "mongoose";
 
 const tweetSchema = new Schema(
     {
-
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        content: {
+            type: String,
+            required: true
+        }
     },
     {timestamps: true}
 )

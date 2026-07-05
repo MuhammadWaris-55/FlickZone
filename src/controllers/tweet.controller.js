e8f2539633da992ac57aca90532c32c5eb7836ec
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-06 00:44:25 +0500

    feat: implement deleteTweet function with validation and authorization checks

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -126,7 +126,27 @@ const updateTweet = asyncHandler(async (req, res) => {
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const { tweetId } = req.params
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    if (tweet.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "You are not authorized to delete this tweet")
+    }
+
+    const deletedTweet = await Tweet.findByIdAndDelete(tweet)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, deletedTweet, "Tweet deleted successfully"))
 })
 
 export {

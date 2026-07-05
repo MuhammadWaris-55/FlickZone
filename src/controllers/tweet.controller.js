4137ba0b0943a73769defef3e42f27ea57022d07
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-06 00:10:42 +0500

    feat: implement createTweet function for tweet creation

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,6 +7,25 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+
+    const { content } = req.body
+
+    if (!content || content.trim() === "") {
+        throw new ApiError(400, "Content is required")
+    }
+
+    const tweet = await Tweet.create({
+        content,
+        owner: req.user?._id
+    })
+
+    if (!tweet) {
+        throw new ApiError(500, "Something went wrong while creating the tweet")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet created successfully"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {

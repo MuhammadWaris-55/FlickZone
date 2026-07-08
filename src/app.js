1fefaaf3f3ff57382dfbcec361893314a0d99309
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-08 16:16:50 +0500

    feat: add comments route to the application

--- src/app.js
+++ src/app.js
@@ -34,10 +34,12 @@ app.use(cookieParser()) //the work of cookie parser is that we can access cookie
 //import Routes
 import userRouter from "./routes/user.route.js";
 import tweetRouter from "./routes/tweet.route.js"
+import commentRouter from "./routes/comment.route.js"
 
 //routes declaration
 app.use("/api/v1/users" , userRouter) //using app.use because router is in another file
 //if we go to /users it will redirect us to userRouter
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/comments", commentRouter)
 
 export { app }

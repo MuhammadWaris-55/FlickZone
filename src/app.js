75f881e730e9302cdc57ee0f4d6b166575e29790
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-14 16:35:26 +0500

    feat: add playlist routes to app.js

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ import tweetRouter from "./routes/tweet.route.js"
 import commentRouter from "./routes/comment.route.js"
 import videoRouter from "./routes/video.route.js"
 import likeRouter from "./routes/like.route.js"
+import playlistRouter from "./routes/playlist.route.js"
 
 //routes declaration
 app.use("/api/v1/users", userRouter) //using app.use because router is in another file
@@ -45,5 +46,6 @@ app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 export { app }

1d200da51337f2687e9907cfc0e2427fd5d0218a
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-18 15:55:36 +0500

    feat: add variable fonts and update styles in CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './index.css';
+import { Button } from "@/components/ui/button"
 
 const App = () => {
   return (
     <div>
-      
+     <h1 className="font-heading text-4xl font-bold">FlickZone</h1>
+     <Button>Test Button</Button>
     </div>
   )
 }

e8b09304ca8bb9d06fa5391f98b5eaf3b2373a7f
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-27 19:48:46 +0500

    fix: update import statement to use named import for app in index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,3 @@
 // api/index.js
-import app from "../src/app.js"; // wherever your express app is configured (routes, middleware etc.)
+import { app } from "../src/app.js"; // wherever your express app is configured (routes, middleware etc.)
 export default app;

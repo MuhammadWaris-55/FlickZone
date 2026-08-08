0c91375b39cca20f524c737c33c2519da91c11f1
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-08-08 11:18:07 +0500

    feat: add @vercel/analytics dependency and integrate Analytics component in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import AppRoutes from "@/routes/AppRoutes";
 import { useAuth } from "@/context/AuthContext";
 import AppLoadingScreen from "@/components/AppLoadingScreen";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   const { user, loading, isAuthenticated } = useAuth();
@@ -11,9 +12,10 @@ const App = () => {
   if (loading) return <AppLoadingScreen />;
 
   return (
-   <AppRoutes />
-  
-
+    <>
+      <AppRoutes />
+      <Analytics />
+    </>
   );
 };
 
